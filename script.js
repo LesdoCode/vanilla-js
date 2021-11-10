7dba189a322c1bcccb88807e4a9010420a6dc8c1
Tebogo Lemekoana <lesdocode@gmail.com>
2021-11-10 19:13:59 +0200

    add extra stuff and comments

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-const card5 = document.getElementById("card-5");
-const card6 = document.getElementById("card-6");
+const card5 = document.getElementById("card-5"); //get card 5 from html
+const card6 = document.getElementById("card-6"); //get card 6 from html
 
 let counter = 0;
+let currentColor = "blue";
+
 card5.addEventListener("click", function () {
-	card6.innerHTML += ++counter;
+	card6.innerHTML = ++counter;
+
+	if (currentColor !== "blue") {
+		currentColor = "blue";
+	} else {
+		currentColor = "green";
+	}
+	card5.style = `background-color: ${currentColor};`;
 });
+
+//the above can also be expressed way like like: uncomment it and check out
+/*function card5_click_handler() {
+	card6.innerHTML = ++counter;
+
+	if (currentColor !== "blue") {
+		currentColor = "blue";
+	} else {
+		currentColor = "green";
+	}
+	card5.style = `background-color: ${currentColor};`;
+}
+card5.addEventListener("click", card5_click_handler);*/
+
+//Find all event listeners here: https://developer.mozilla.org/en-US/docs/Web/Events
