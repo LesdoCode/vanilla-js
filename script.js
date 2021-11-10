c6bf1c49af29a866ec9d01fc3f444a1a91804d7a
Tebogo Lemekoana <lesdocode@gmail.com>
2021-11-10 19:00:54 +0200

    Loadshedding

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 const card5 = document.getElementById("card-5");
+const card6 = document.getElementById("card-6");
+
+let counter = 0;
+card5.addEventListener("click", function () {
+	card6.innerHTML += ++counter;
+});
