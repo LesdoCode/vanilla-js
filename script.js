135d1a3022d30f22fad1c60813cf248ca4290311
Tebogo Lemekoana <lesdocode@gmail.com>
2021-11-18 22:54:44 +0200

    Full game working

--- script.js
+++ script.js
@@ -21,10 +21,36 @@ function gridClickEventHandler(event) {
 	if (!clickedButton.classList.contains("card")) return;
 	if (clickedButton.classList.contains("played")) return;
 
-	clickedButton.classList.add("played");
 	clickedButton.innerHTML = getNextPlayer();
+	clickedButton.classList.add("played");
+
+	if (isWin(grid, currentPlayer)) {
+		alert(currentPlayer + " won!");
+		clearGrid(grid);
+	}
 }
 
-function isWin(grid, player) {}
+function isWin(grid, player) {
+	const currentGrid = [];
+	for (let card of grid.children) {
+		currentGrid.push(card.innerHTML);
+	}
+	for (let combo of winningCombinations) {
+		if (
+			currentGrid[combo[0]] === player &&
+			currentGrid[combo[1]] === player &&
+			currentGrid[combo[2]] === player
+		)
+			return true;
+	}
+	return false;
+}
+
+function clearGrid(grid) {
+	for (let card of grid.children) {
+		card.innerHTML = "";
+		card.classList.remove("played");
+	}
+}
 
 grid.addEventListener("click", gridClickEventHandler);
